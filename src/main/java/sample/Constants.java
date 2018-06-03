393c0dfa7c92a6b7b1a32e89ddc66ac1fe5a0af3
Samim Hakimi <30713537+sammyHa@users.noreply.github.com>
2018-06-03 11:09:10 -0700

    Update Constants.java

--- src/main/java/sample/Constants.java
+++ src/main/java/sample/Constants.java
@@ -19,8 +19,8 @@ public class Constants {
     private final static String PHONE_NUMBER = " +18582120560";
     private final static String MY_PHONE_NUMBER = " +100000000";
 
-    private final static String ACCOUNT_SID = "ACf5d09859f655b8879a3613251424e673";
-    private final static String AUTH_TOKEN = "bb0ff630d6666a76b64495d4cece2621";
+    private final static String ACCOUNT_SID = "ACxxxxxxxxxxxxx";
+    private final static String AUTH_TOKEN = "Your_Auth_Token";
 
     public  void TwilioMessage() {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
